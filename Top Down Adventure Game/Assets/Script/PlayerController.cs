a4a85fce4fdbc056d05f54d8943b61c0537caea6
tassbash <92358385+tassbash@users.noreply.github.com>
2023-10-06 13:35:01 -0400

    added key

--- Top Down Adventure Game/Assets/Script/PlayerController.cs
+++ Top Down Adventure Game/Assets/Script/PlayerController.cs
@@ -6,6 +6,9 @@ using UnityEngine.SceneManagement; //importing SceneManagement library
 public class PlayerController : MonoBehaviour
 {
     public float speed = 0.5f;
+    public bool hasKey = false;
+
+    public GameObject key;
 
     // Start is called before the first frame update
     void Start()
@@ -51,6 +54,13 @@ public class PlayerController : MonoBehaviour
         {
             Debug.Log("hit");
             SceneManager.LoadScene(1); //access SceneManager class for LoadScene function
+        }
+
+        if (collision.gameObject.tag.Equals("key"))
+        {
+            Debug.Log("obtained key");
+            key.SetActive(false); //key disappears
+            hasKey = true; //player has the key now
         }
     }
 }
